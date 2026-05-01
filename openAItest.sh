cbc3ddc2f33ef157d09c8b0e8886d600b3b29cf0
infrafast <info@infrafast.com>
2026-05-01 12:05:38 +0200

    test script

--- openAItest.sh
+++ openAItest.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-set -e
-
-export OPENAI_API_KEY="$OPENAI_API_KEY"
-
 curl https://api.openai.com/v1/responses \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer ${OPENAI_API_KEY}" \
   -d @- <<'JSON'
 {
   "model": "gpt-5.1",
-  "input": "Lis le nom du canal 3 de la X32.",
+  "input": "Lis le volume du main LR.",
   "tools": [
     {
       "type": "mcp",
@@ -19,8 +15,6 @@ curl https://api.openai.com/v1/responses \
       "allowed_tools": [
         "x32_get_channel_name",
         "x32_get_main_fader",
-        "x32_set_channel_bus_send_level",
-        "x32_set_channel_bus_send_on",
         "x32_mute_main",
         "x32_set_main_fader"
       ],
